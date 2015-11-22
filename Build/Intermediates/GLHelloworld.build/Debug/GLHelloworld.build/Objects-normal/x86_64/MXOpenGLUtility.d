b00b4be14d859ea1f4275a0995cf5b6e1801e9d8
FlyApple <silly@Sillys-MacBook-Pro.local>
2015-11-22 23:00:57 +0800

    fix file name;

---
+++ Build/Intermediates/GLHelloworld.build/Debug/GLHelloworld.build/Objects-normal/x86_64/MXOpenGLUtility.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/silly/Private/GLHelloworld/GLHelloworld/MXOpenGLUtility.cpp \
+  /Users/silly/Private/GLHelloworld/GLHelloworld/MXOpenGLPrecompile.h \
+  include/GL/glew.h \
+  /Users/silly/Private/GLHelloworld/GLHelloworld/MXOpenGLUtility.h \
+  /Users/silly/Private/GLHelloworld/GLHelloworld/MXOpenGLEngine.h
